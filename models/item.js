1ff9c4ea8ad7235bb8144564f5acd689bedab72b
Ben Chou <ben7152000@gmail.com>
2021-01-17 14:41:49 +0800

    update seeder

--- models/item.js
+++ models/item.js
@@ -4,6 +4,38 @@ const itemSchema = new Schema({
   name: {
     type: String,
     required: true
+  },
+  name_en: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  google_map: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
   }
 })
 module.exports = mongoose.model('Item', itemSchema)
