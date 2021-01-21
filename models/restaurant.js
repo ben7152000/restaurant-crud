aed47f9b2f08a297216da2d7e476c3eddae75baf
Ben Chou <ben7152000@gmail.com>
2021-01-21 17:00:58 +0800

    updata restaurant seeder

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,18 +1,16 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+
 const restaurantSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  name_en: String,
-  category: String,
-  image: String,
+  name: { type: String, required: true },
+  name_en: { type: String, lowercase: true, trim: true },
+  category: { type: String, default: '未分類' },
+  image: { type: String, default: 'http://lorempixel.com/400/200/food/' },
   location: String,
-  phone: String,
+  phone: { type: String, match: /[0-9]/ },
   google_map: String,
-  rating: String,
-  description: String
+  rating: { type: Number, min: 1, max: 5, default: 3.0 },
+  description: { type: String, default: '非常美味' }
 })
 
 module.exports = mongoose.model('Restaurant', restaurantSchema)
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,23 +1,9 @@
 const Restaurant = require('../restaurant')
-const restaurant = require('../../restaurantItem.json')
-
+const restaurant = require('./restaurant.json')
 const db = require('../../config/mongoose')
 
 db.once('open', () => {
-  console.log('The database is working')
-  restaurant.forEach(restaurantItem => {
-    Restaurant.create({
-      id: restaurantItem.id,
-      name: restaurantItem.name,
-      name_en: restaurantItem.name_en,
-      category: restaurantItem.category,
-      image: restaurantItem.image,
-      location: restaurantItem.location,
-      phone: restaurantItem.phone,
-      google_map: restaurantItem.google_map,
-      rating: restaurantItem.rating,
-      description: restaurantItem.description
-    })
-      .then(() => db.close())
-  })
+  for (let i = 0; i < restaurant.results.length; i++) {
+    Restaurant.create(restaurant.results[i])
+  }
 })
