5a36d045f8f6c102b9515902ebc68ec2a6274319
Ben Chou <ben7152000@gmail.com>
2021-01-28 23:12:10 +0800

    fix control

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 const home = require('./modules/home')
-const detail = require('./modules/detail')
 const search = require('./modules/search')
-const controll = require('./modules/controll')
+const control = require('./modules/control')
 
 router.use('/', home)
-router.use('/restaurants', controll)
-router.use('/restaurants', detail)
+router.use('/restaurants', control)
 router.use('/search', search)
 
 module.exports = router
--- routes/modules/control.js
+++ routes/modules/control.js
@@ -99,4 +99,13 @@ router.post('/:id/deleteRestaurant', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// detail
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurantItem) => res.render('../views/restaurant/detail', { restaurantItem }))
+    .catch(error => console.log(error))
+})
+
 module.exports = router
