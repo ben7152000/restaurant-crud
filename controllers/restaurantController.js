28f4119d1751e06ae37bb2499c4bb3e91cd3b708
Ben Chou <ben7152000@gmail.com>
2021-03-13 07:35:48 +0800

    update

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -18,6 +18,7 @@ const restaurantController = {
   },
   editRestaurantPage: (req, res) => {
     const id = req.params.id
+    console.log(id)
     return Restaurant.findById(id)
       .lean()
       .then((restaurantItem) => res.render('../views/restaurants/edit', { restaurantItem }))
@@ -46,6 +47,26 @@ const restaurantController = {
       .lean()
       .then((restaurantItem) => res.render('../views/restaurants/detail', { restaurantItem }))
       .catch(error => console.log(error))
+  },
+  searchRestaurant: (req, res) => {
+    const keyword = req.query.keyword
+    return Restaurant.find({
+      $or: [
+        { name: { $regex: `${keyword}`, $options: '$i' } },
+        { category: { $regex: `${keyword}`, $options: '$i' } }
+      ]
+    })
+      .lean()
+      .then(restaurant => res.render('../views/restaurants/index', { restaurant, keyword }))
+  },
+  sortRestaurant: (req, res) => {
+    const sort = req.body.sort
+    const order = req.body.order
+    Restaurant.find()
+      .lean()
+      .sort({ [sort]: order })
+      .then(restaurant => res.render('../views/restaurants/index', { restaurant }))
+      .catch(error => console.error(error))
   }
 }
 
--- routes/routes.js
+++ routes/routes.js
@@ -9,14 +9,19 @@ const restaurantController = require('../controllers/restaurantController')
 // middleware
 const { authenticator } = require('../middlewares/auth')
 
+// home
+router.get('/', (req, res) => res.redirect('/users/login'))
+
 // restaurant
 router.get('/restaurants', authenticator, restaurantController.getRestaurants)
+router.post('/restaurants', authenticator, restaurantController.sortRestaurant)
 router.get('/restaurants/create', authenticator, restaurantController.createRestaurantPage)
 router.post('/restaurants/create', authenticator, restaurantController.createRestaurant)
 router.get('/restaurants/:id/edit', authenticator, restaurantController.editRestaurantPage)
 router.post('/restaurants/:id/edit', authenticator, restaurantController.editRestaurant)
 router.post('/restaurants/:id/delete', authenticator, restaurantController.deleteRestaurant)
 router.get('/restaurants/:id', authenticator, restaurantController.detailRestaurant)
+router.get('/restaurants/search', authenticator, restaurantController.searchRestaurant)
 
 // users
 router.get('/users/login', userController.loginPage)
